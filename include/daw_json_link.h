e486e6f9569ddf010465b967fc5de9124b38ae8f
Darrell Wright <dwright-git@dawdevel.ca>
2016-07-18 22:01:59 -0400

    .

--- include/daw_json_link.h
+++ include/daw_json_link.h
@@ -33,6 +33,7 @@
 #include <stdexcept>
 #include <string>
 #include <unordered_map>
+#include <map>
 
 #include "daw_json.h"
 #include "daw_json_parser.h"
@@ -169,7 +170,8 @@ namespace daw {
 
 
 			std::string m_name;
-			std::unordered_map<impl::string_value, data_description_t> m_data_map;
+			//std::unordered_map<impl::string_value, data_description_t> m_data_map;
+			std::map<impl::string_value, data_description_t> m_data_map;
 
 			template<typename T>
 			JsonLink & link_value( boost::string_ref name, T& value ) {
